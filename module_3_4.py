2e52f1382bf660a6a7c46178d3982c834f6ba0b0
flask599 <flask599@gmail.com>
2024-12-16 00:57:34 +0300

    См/работа по уроку "Произвольное число параметров"

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+print('Однокоренные')
+print('-------------------------')
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if root_word.upper() in str(i).upper() or str(i).upper() in root_word.upper():
+           same_words.append(i)
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
