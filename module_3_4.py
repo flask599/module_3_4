e6eda2aa5305146b0cb2ebaec1cf813c47004584
flask599 <flask599@gmail.com>
2024-12-16 01:01:47 +0300

    См/работа по уроку "Произвольное число параметров"

--- module_3_4.py
+++ module_3_4.py
@@ -1,6 +1,8 @@
 print('Однокоренные')
 print('-------------------------')
-
+'''''
+решение методом ключевых слов: in
+'''''
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
