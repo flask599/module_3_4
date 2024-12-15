68e19890f0d63ec195c1d90b80ad760b19c5ebae
flask599 <flask599@gmail.com>
2024-12-16 01:00:33 +0300

    Add files via upload

--- module_3_4_1.py
+++ module_3_4_1.py
@@ -1,6 +1,8 @@
 print('Однокоренные')
 print('-------------------------')
-
+'''''
+решение методом строк: count 
+'''''
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
